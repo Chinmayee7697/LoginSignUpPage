fa98262fb8c1955e8574dde7ef8a65d37acb4d4d
Chinmayee Sharma <chinmayeesharma911@gmail.com>
2024-05-05 19:54:45 +0530

    .env file added

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0Mc04Enalk2j7ezeQNrnphZISGTZ89Cg",
-  authDomain: "login-signup-976f4.firebaseapp.com",
-  projectId: "login-signup-976f4",
-  storageBucket: "login-signup-976f4.appspot.com",
-  messagingSenderId: "742840698339",
-  appId: "1:742840698339:web:5a79263319dec3d4778cd6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
